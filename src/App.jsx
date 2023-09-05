4db8ae0e718e435882e65e3d1a0420272414a1d7
Lauren Bleyl <laurenbleyl@gmail.com>
2023-09-05 00:17:46 -0500

    fixing pathing

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ export default function App() {
   return (
     <>
     <Navigation></Navigation>
-    {/* <AboutMe></AboutMe> */}
+    <AboutMe></AboutMe>
     <Portfolio></Portfolio>
     <Footer></Footer>
     </>
